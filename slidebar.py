44431b0771e05c66353cff56fbe4355c405d39cf
0xdeciverAngel <31663805+0xdeciverAngel@users.noreply.github.com>
2020-12-18 01:39:06 +0800

    add slidebar

---
+++ slidebar.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+
+browser = webdriver.Chrome()
+browser.get('http://www.porters.vip/captcha/sliders.html')
+hover = browser.find_element_by_css_selector('.hover')
+
+action = webdriver.ActionChains(browser)
+action.click_and_hold(hover).perform()  
+action.move_by_offset(340, 0)  
+action.release().perform() 
